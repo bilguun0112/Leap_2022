b8f969c19b01c3b9f0598c29484622e408ddb2bc
ruzishdee <bilguunbayanmunkh10@gmail.com>
2022-12-08 22:18:45 +0800

    fibo

--- Js assignment/Js/fibo.js
+++ Js assignment/Js/fibo.js
@@ -1,9 +1,12 @@
-// let f1 = 0, f2 = 1, n = 0;
-// let m = Number(prompt("Gimme number"));
+let f1 = 0,
+    f2 = 1,
+    n = 0;
+let m = Number(prompt("Gimme number"));
 
-// for ( ;0 <= n <= m ; n++) {
-//     f3 = f1 + f2 ;
-//     f1 = f2;
-//     f2 = f3;
-//     console.log(f1);
-// }
+for (; n <= m; n++) {
+    console.log(f1);
+    f3 = f1 + f2;
+    f1 = f2;
+    f2 = f3;
+
+}
