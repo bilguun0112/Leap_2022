7461f0bc32491371fa599bb2df61e95838c9ba7d
ruzishdee <bilguunbayanmunkh10@gmail.com>
2022-12-12 20:00:06 +0800

    color changer fixed

--- JS-DOM/Assignment/JS/ex03.js
+++ JS-DOM/Assignment/JS/ex03.js
@@ -1,16 +1,3 @@
-// let green = document.querySelector("img").src = "./images/green.jfif";
-
-// document.getElementsByClassName("colorGreen").addEventListener("click", green)
-
-
-let green = document.querySelector("colorGreen");
-
-green.addEventListener("click", () => {
-    document.querySelector("img").style = "width: 100%"
-})
-
-// document.querySelector("img").src = "./images/green.jfif";
-
 function changeGreen() {
     let image = document.getElementById('clothes');
     image.src = "./images/green.jfif";
