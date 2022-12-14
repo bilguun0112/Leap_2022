8b190f77fcf23dab12f82ad84fe1d99e3aa4dbae
Morning <morning@mstars-lab2-10s-iMac.local>
2022-12-14 12:42:17 +0800

    day-22

--- JS-DOM/Assignment/JS/randomcolor.js
+++ JS-DOM/Assignment/JS/randomcolor.js
@@ -1,8 +1,8 @@
-const randColor = () =>  {
-    let ranColor = document.getElementsByTagName("body");
-    ranColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-    document.getElementById("color").style.background-color = "randColor()"
-}
+// const randColor = () =>  {
+//     let ranColor = document.getElementsByTagName("body").style;
+//     let c = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+//     ranColor.background-color =
+// }
 
 // let colorButton = document.querySelector("#color");
 
@@ -10,3 +10,7 @@ const randColor = () =>  {
 //     document.getElementsByClassName("changeBack")[0].style = "background-color: red"
 //     document.getElementById("color").style = "background-color: randColor()"
 // });
+
+// let randomColor = document.getElementById('randomNumber');
+// let color = document.getElementsByName('body').style;
+// randomColor.addEventListener('click', color.background-color = randColor)
